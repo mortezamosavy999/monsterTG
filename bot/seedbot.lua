1cf0ff75439fbd0b2b4614ca63ccd3212e165c66
BlackHat(س.مرتضی) <www.mortezamosavy0910@gmail.com>
2016-02-04 12:11:09 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -235,20 +235,11 @@ An advance Administration bot based on yagop/telegram-bot
 https://github.com/SEEDTEAM/TeleSeed
 
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
+@blackhat_admin02
+@blackhat_admin03
 
 Our channels
-@teleseedch [English]
-@iranseed [persian]
+@blackhatchannel
 ]],
     help_text_realm = [[
 Realm Commands:
